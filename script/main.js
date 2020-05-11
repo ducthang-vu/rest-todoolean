95b8a70842cf036a76b3c1420322ca4151fa5196
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-11 14:50:57 +0200

    function printListItems working

--- script/main.js
+++ script/main.js
@@ -1,6 +1,36 @@
 console.log('main.js is working')
 console.log('$')
 
+
+function getAjaxSetting(method) {
+    return {
+        url: '157.230.17.132:3025/todos',
+        method: method,
+    }
+}
+
+
+function printListItems(template, array_of_objects) {
+    array_of_objects.forEach(object => $('.my-list').append(template(object)))
+}
+
+/*
+function printAll(ajaxSetting) {
+    $.$.ajax(ajaxSetting)
+        .done(function(result) {
+            for (let item of result) {
+
+            }
+        })
+}
+*/
+
 $(document).ready(function () {
-    
+    // Init Handlebars
+    var source = $('#entry-template').html();
+    var template = Handlebars.compile(source);
+
+    //printAll()
+
+    printListItems(template, [{text: 'prova'}])
 });
